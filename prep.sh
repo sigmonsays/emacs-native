ff960b650648e43ba50a11a44b4e5f442001c6f5
Sig Lange <sig.lange@gmail.com>
2022-01-21 15:46:48 -0800

    faster git clone

--- prep.sh
+++ prep.sh
@@ -2,10 +2,8 @@
 
 cd "$(dirname "$0")"
 
-git clone git://git.savannah.gnu.org/emacs.git
+git clone -b emacs-28.0.91 --single-branch --depth 1  git@github.com:emacs-mirror/emacs.git
 cd emacs
-git branch -D build
-git checkout -b build emacs-28.0.91
 rm -rf .git
 cd ..
 mv emacs/* emacs/.* .
