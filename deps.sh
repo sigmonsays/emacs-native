40a4a69eef24f97033942754c3dd885d215df7f7
Sig Lange <sig.lange@gmail.com>
2022-08-02 21:16:41 -0700

    tweaks

--- deps.sh
+++ deps.sh
@@ -1,4 +1,5 @@
 
+sudo apt-get update -qq
 sudo apt install -y  libxpm-dev libjpeg-dev libgnutls28-dev libgif-dev
 sudo apt install -y  libtiff-dev libacl1-dev libgtk-3-dev libwebkit2gtk-4.0-dev
 sudo apt install -y  librsvg2-dev libmagickcore-dev libmagick++-dev
--- prep.sh
+++ prep.sh
@@ -3,4 +3,6 @@
 cd "$(dirname "$0")"
 
 [ -z emacs ] && exit 0
-git clone -b emacs-28.0.91 --single-branch --depth 1  git@github.com:emacs-mirror/emacs.git
+git clone -b emacs-28.0.91 \
+    --single-branch --depth 1 \
+    git@github.com:emacs-mirror/emacs.git
